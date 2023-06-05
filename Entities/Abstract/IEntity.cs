575c4ac51122614cff8deab61b27904736cb911e
Süleyman Beyaz <suleymanbeyaz10@gmail.com>
2023-06-05 11:06:52 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    //IENtity implement eden class bir veritabanı tabanıdır.
+    public interface IEntity
+    {
+    }
+}
