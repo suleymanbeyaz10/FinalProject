f01c425041db293153fca578e960d272c000bb51
Süleyman Beyaz <suleymanbeyaz10@gmail.com>
2023-06-06 11:23:45 +0300

    Entity Framework added and the project is configured.  6.6.2023

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace DataAccess.Concrete.InMemory
 {
@@ -14,11 +15,11 @@ namespace DataAccess.Concrete.InMemory
         {
             _products = new List<Product> {
 
-                new Product{ ProductId = 1, CategoryId = 1, ProductName = "Bardak", UnitPrice = 15, UnitInStock = 15},
-                new Product{ ProductId = 2, CategoryId = 1, ProductName = "Tabak", UnitPrice = 40, UnitInStock = 3},
-                new Product{ ProductId = 3, CategoryId = 2, ProductName = "Telefon", UnitPrice = 1500, UnitInStock = 2},
-                new Product{ ProductId = 4, CategoryId = 2, ProductName = "Klavye", UnitPrice = 150, UnitInStock = 65},
-                new Product{ ProductId = 5, CategoryId = 2, ProductName = "Fare", UnitPrice = 85, UnitInStock = 1}  
+                new Product{ ProductId = 1, CategoryId = 1, ProductName = "Bardak", UnitPrice = 15, UnitsInStock = 15},
+                new Product{ ProductId = 2, CategoryId = 1, ProductName = "Tabak", UnitPrice = 40, UnitsInStock = 3},
+                new Product{ ProductId = 3, CategoryId = 2, ProductName = "Telefon", UnitPrice = 1500, UnitsInStock = 2},
+                new Product{ ProductId = 4, CategoryId = 2, ProductName = "Klavye", UnitPrice = 150, UnitsInStock = 65},
+                new Product{ ProductId = 5, CategoryId = 2, ProductName = "Fare", UnitPrice = 85, UnitsInStock = 1}  
             };
         }
         public List<Product> GetAll()
@@ -48,13 +49,23 @@ namespace DataAccess.Concrete.InMemory
             productToUpdate.ProductName = product.ProductName;
             productToUpdate.CategoryId = product.CategoryId;
             productToUpdate.UnitPrice = product.UnitPrice;
-            productToUpdate.UnitInStock = product.UnitInStock;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
             
         }
 
         public List<Product> GetAllByCategory(int CategoryId)
         {
             return _products.Where(p => p.CategoryId == CategoryId).ToList();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
         }
     }
 }
