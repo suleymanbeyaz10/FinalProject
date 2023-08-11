664ab6df1ed0387b4f6a378464cb1e9440678d37
Süleyman Beyaz <suleymanbeyaz10@gmail.com>
2023-08-11 12:31:41 +0300

    exception middleware , error system for frontend

--- Core/Extensions/ErrorDetails.cs
+++ Core/Extensions/ErrorDetails.cs
@@ -1,9 +1,8 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using FluentValidation.Results;
+using Newtonsoft.Json;
 
 namespace Core.Extensions
 {
@@ -11,10 +10,15 @@ namespace Core.Extensions
     {
         public string Message { get; set; }
         public int StatusCode { get; set; }
-
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
         }
     }
+
+    public class ValidationErrorDetails : ErrorDetails
+    {
+        public IEnumerable<ValidationFailure> ValidationErrors { get; set; }
+    }
+
 }
--- Core/Extensions/ExceptionMiddleware.cs
+++ Core/Extensions/ExceptionMiddleware.cs
@@ -1,11 +1,11 @@
-﻿using FluentValidation;
-using Microsoft.AspNetCore.Http;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Http;
 
 namespace Core.Extensions
 {
@@ -36,9 +36,20 @@ namespace Core.Extensions
             httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             string message = "Internal Server Error";
+            IEnumerable<ValidationFailure> errors;
             if (e.GetType() == typeof(ValidationException))
             {
                 message = e.Message;
+                errors = ((ValidationException)e).Errors;
+                httpContext.Response.StatusCode = 400;
+
+                return httpContext.Response.WriteAsync(new ValidationErrorDetails
+                {
+                    StatusCode = 400,
+                    Message = message,
+                    ValidationErrors = errors
+                }.ToString());
+
             }
 
             return httpContext.Response.WriteAsync(new ErrorDetails
